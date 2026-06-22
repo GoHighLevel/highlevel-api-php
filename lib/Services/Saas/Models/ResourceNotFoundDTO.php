5befb537920f2adee51c9471fad7a05b891dc044
Karan Kaneria <karankumar.kaneria@gohighlevel.com>
2026-06-22 18:27:06 +0530

    fix: auto generated comment added for the generated files

--- lib/Services/Saas/Models/ResourceNotFoundDTO.php
+++ lib/Services/Saas/Models/ResourceNotFoundDTO.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * @generated
+ * File generated from our OpenAPI spec
+ */
+
 namespace HighLevel\Services\Saas\Models;
 
 /**
--- lib/Services/Saas/Saas.php
+++ lib/Services/Saas/Saas.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * @generated
+ * File generated from our OpenAPI spec
+ */
+
 namespace HighLevel\Services\Saas;
 
 use HighLevel\HighLevel;
